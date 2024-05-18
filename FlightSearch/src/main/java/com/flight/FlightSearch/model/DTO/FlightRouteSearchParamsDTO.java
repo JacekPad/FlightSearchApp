1c1eea287db23fb33e73af9d8048c86cb578b9a5
JacekPad <jacekpadyjasek@gmail.com>
2024-05-18 19:08:16 +0200

    custom DTO validator

--- FlightSearch/src/main/java/com/flight/FlightSearch/model/DTO/FlightRouteSearchParamsDTO.java
+++ FlightSearch/src/main/java/com/flight/FlightSearch/model/DTO/FlightRouteSearchParamsDTO.java
@@ -2,6 +2,8 @@ package com.flight.FlightSearch.model.DTO;
 
 import com.flight.FlightSearch.model.enums.FlightClass;
 import com.flight.FlightSearch.model.enums.FlightType;
+import com.flight.FlightSearch.utils.validators.DateValidatorConstraint;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,14 +11,46 @@ import java.time.LocalDateTime;
 @Data
 public class FlightRouteSearchParamsDTO {
 
+    @NotNull
     private FlightClass flightClass;
+
+    @NotNull
+    @Size(min = 3, max = 3, message = "Iata code must be 3 characters long")
     private String departureAirportIata;
+
+    @NotNull
+    @Size(min = 3, max = 3, message = "Iata code must be 3 characters long")
     private String arrivalAirportIata;
+
+    @NotNull
+    @DateValidatorConstraint
     private String departureDate;
+
+    @Null
+    @DateValidatorConstraint
     private String returnDate;
+
+    @NotNull
+    @Size(min = 5, max = 6, message = "Flight type must be between 5 and 6 characters")
     private FlightType flightType;
+
+    @NotNull
+    @Max(value = 5, message = "Maximum number of stops is 5")
+    @Min(value = 1, message = "Number of stops must be at least 1")
     private int maxStops;
+
+    @NotNull
+    @Max(value = 5, message = "Maximum number of adults is 5")
+    @Min(value = 1, message = "At least one adult is required")
     private int adult;
+
+    @NotNull
+    @Max(value = 5, message = "Maximum children of adults is 5")
+    @Min(value = 0, message = "Value can not be negative")
     private int child;
+
+    @NotNull
+    @Max(value = 5, message = "Maximum number of infants is 5")
+    @Min(value = 0, message = "Value can not be negative")
     private int infant;
 }
---
+++ FlightSearch/src/main/java/com/flight/FlightSearch/utils/validators/DateValidator.java
@@ -0,0 +1,22 @@
+package com.flight.FlightSearch.utils.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+import java.time.LocalDateTime;
+
+public class DateValidator implements ConstraintValidator<DateValidatorConstraint, String> {
+    @Override
+    public void initialize(DateValidatorConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        String regex = "^20\\d{2}-(0[1-9]|1[0-2])-[0-3][0-9]T1[0-9]|2[0-4]:[0-5][0-9]:[0-5][0-9]$";
+        return value.matches(regex);
+    }
+}
